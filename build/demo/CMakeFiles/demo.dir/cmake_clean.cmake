c30859b10e24186579a01c54ab646790d57c4df1
abhi <abhitumm@iu.edu>
2017-11-15 18:16:03 -0500

    1st

---
+++ build/demo/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/src/demo.cpp.o"
+  "/home/abhi/Desktop/ros_demo/devel/lib/ros_demo/demo.pdb"
+  "/home/abhi/Desktop/ros_demo/devel/lib/ros_demo/demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
